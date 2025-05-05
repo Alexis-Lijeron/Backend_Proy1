fcc1f2c5af836bd834d82b20b88f0100218b903c
Alexis-lijeron <alexislijeron.a@gmail.com>
2025-05-05 11:47:28 -0400

    cors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from app.api import users, chats, contextos, mensajes, feedbacks
 from app.rag import chroma_routes, rag_routes, chroma_metadatos_routes, consulta_txt
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Sistema de Chats Multi-Contexto",
     description="API para gestionar usuarios, chats, contextos, mensajes y feedbacks",
     version="1.0.0",
 )
-
+# ----------- CORS -----------
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En producción puedes poner ["https://tudominio.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(users.router)
 app.include_router(chats.router)
 app.include_router(contextos.router)
